1e8188b825338ebbbf417946de98b9142ae326c4
Никитос <nikitadidenko222@gmail.com>
2023-11-12 19:46:01 +0200

    hm5

--- app/currency/models.py
+++ app/currency/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 class Rate(models.Model):
-    buy = models.DecimalField(..., max_digits=5, decimal_places=2)
-    sell = models.DecimalField(..., max_digits=5, decimal_places=2)
-    type = models.DateField()  # usd, euro, etc
-    created = models.CharField(max_length='3')
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    sell = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField()  # usd, euro, etc
+    type = models.CharField(max_length=3)
+    source = models.CharField(max_length=255)
+
+
+class ContactUs(models.Model):
+    email = models.EmailField(max_length=254)
+    subject = models.TextField(verbose_name='subject')
+    message = models.TextField(verbose_name='message')
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from app.currency.views import hello
+from app.currency.views import rate_list, message
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/world', hello)
+    path('rate/list', rate_list),
+    path('contactus/list', message),
 ]
+
