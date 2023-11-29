82defd2f9279002d168721f877e943e5f25111d8
Никитос <nikitadidenko222@gmail.com>
2023-11-29 20:48:07 +0200

    hm_8

---
+++ app/currency/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+from app.currency.models import Rate, ContactUs, Source
+
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Rate
+        fields = (
+            'buy',
+            'sell',
+            'change_type',
+            'source',
+        )
+
+
+class MessageForms(forms.ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = (
+            'email',
+            'subject',
+            'message',
+        )
+
+
+class SourceForm(forms.ModelForm):
+    class Meta:
+        model = Source
+        fields = (
+            'source_url',
+            'source_name',
+        )
--- app/currency/models.py
+++ app/currency/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Rate(models.Model):
     buy = models.DecimalField(max_digits=5, decimal_places=2)
     sell = models.DecimalField(max_digits=5, decimal_places=2)
-    created = models.DateTimeField()  # usd, euro, etc
+    created = models.DateTimeField(auto_now_add=True)  # usd, euro, etc
     change_type = models.CharField(max_length=3)
     source = models.CharField(max_length=255)
 
@@ -13,3 +13,9 @@ class ContactUs(models.Model):
     email = models.EmailField(max_length=254)
     subject = models.TextField(verbose_name='subject')
     message = models.TextField(verbose_name='message')
+
+
+class Source(models.Model):
+    source_url = models.URLField(max_length=255)
+    source_name = models.CharField(max_length=64)
+    created = models.DateTimeField(auto_now_add=True)
